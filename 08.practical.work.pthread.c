9b2c7fd887eb12b7504e3606b8076d3e7f5f739c
HoangHai2401 <tenthednaochadc@gmail.com>
2017-02-17 20:12:38 +0700

    Update 08.practical.work.pthread.c

--- 08.practical.work.pthread.c
+++ 08.practical.work.pthread.c
@@ -5,9 +5,9 @@
 +#define BUFFER_SIZE 10
 +
 +typedef struct {
-+     char type;
-+     int amount;
-+     char unit;
++     char type; // 0=fried chicken, 1=French fries
++     int amount; // pieces or weight
++     char unit;  // 0=piece, 1=gram
 +}item;
 +
 +item bufer[BUFFER_SIZE];
@@ -29,53 +29,5 @@
 +       memcpy(i, &buffer[last], sizeof(item));
 +       last = (last+1) % BUFFER_SIZE;
 +       return i;
-+}
-+
-+void *producepthread(void *param){
-+     item item_x;
-+     item_x.type = '1';
-+     item_x.amount = 0;
-+     item_x.unit = '1';
-+
-+     printf("\Producing item x: type=%c amount=%d unit=%c\n",item_x.type,item_x.amount,item_x.unit);
-+     produce(&item_x);
-+     printf("first = %d last =%d\n,first,last);
-+
-+     item item_y;
-+     item_y.type = '0';
-+     item_y.amount = 1;
-+     item_y.unit = '0';
-+
-+     printf("\Producing item y: type=%c amount=%d unit=%c\n",item_y.type,item_y.amount,item_y.unit);
-+     produce(&item_y);
-+     printf("first = %d last =%d\n,first,last);
-+
-+     item item_z;
-+     item_z.type = '1';
-+     item_z.amount = 1;
-+     item_z.unit = '0';
-+
-+     printf("\Producing item z: type=%c amount=%d unit=%c\n",item_z.type,item_z.amount,item_z.unit);
-+     produce(&item_z);
-+     printf("first = %d last =%d\n,first,last);
-+}
-+
-+void *consumethread(void *param){
-+
-+     printf("After consumed once:\n");
-+     consume();
-+     printf("first = %d last =%d\n,first,last);
-+     printf("After consumed twice:\n");
-+     consume();
-+     printf("first = %d last =%d\n,first,last);
-+}
-+
-+int main(){
-+     pthread_t p1, c1;  
-+     pthread_create(&p1,NULL,producethread,NULL);
-+     pthread_join(p1,NULL);
-+     pthread_create(&c1,NULL,consumethread,NULL);
-+     pthread_join(c1,NULL);
-+     return 0;
 +}
 +
