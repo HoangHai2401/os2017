7ed4e7fb4199a472a1b07056ff054c6e7122ca2c
HoangHai2401 <tenthednaochadc@gmail.com>
2017-02-21 15:01:33 +0700

    Update 04.practical.work.fork.exec.c

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -1,23 +1,25 @@
 +include<stdio.h>
 +include<unistd.h>
++
 +int main(){
-+     print("Main before fork()\n")
-+     int pid=fork();
-+     if (pid == 0){
-+           printf("Im child after fork(), lauching ps -ef\n");
++     print("Main before fork()\n");
++     int pidps = fork();
++     if (pidps == 0){
++           printf("\nIm child after fork(), lauching ps -ef\n");
 +           char *args[]={"/bin/ps","-ef",NULL};
 +           execvp("/bin/ps",args);
 +           printf("Finished launching ps -ef\n");
 +     }
-+     else printf("Im parent after fork(), child is %d\n",pid);
-+     pid = fork();
++     else printf("\nIm parent after fork(), child is %d\n",pidps);
++
++     int pidfree = fork();
 +     
-+     if (pid ==0){
++     if (pidfree == 0){
 +           printf("Im child after fork(), launching free -h\n"); 
 +           char *args[]={"free","-h",NULL};
 +           execvp("free",args);
 +           printf("Finished launching free -h\n");
 +     }
-+     else printf("Im parent afte fork(), child is %d\n, pid);
++     else printf("\nIm parent after fork(), child is %d\n", pidfree);
 +     return 0;
 +}
